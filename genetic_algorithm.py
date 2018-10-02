21b3b48effb3309e2c14802074868d568771620b
Louis-Saglio <louis.saglio@sfr.fr>
2018-10-02 09:10:56 +0200

    Fix bug in Being.decode

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,4 +1,5 @@
 import math
+
 from neural_network import Network, Neuron
 
 
@@ -9,13 +10,25 @@ def sigmoid(x):
 class Being:
     def __init__(self):
         self.genome = (["4589", "7415", "212", "089", "277", "0254"], sigmoid)
+        self.network = Network(self.decode())
+        print(self.network.layers)
 
     def decode(self):
         network = []
         layer = []
-        size = len(self.genome[0])
-        for gene in self.genome:
+        size = len(self.genome[0][0])
+        for gene in self.genome[0]:
             if len(gene) != size:
                 network.append(layer)
                 layer = []
             layer.append(Neuron(int(gene[0]), [int(weight) for weight in gene[1:]], self.genome[1]))
+        network.append(layer)
+        return network
+
+
+def test():
+    b = Being()
+
+
+if __name__ == '__main__':
+    test()
