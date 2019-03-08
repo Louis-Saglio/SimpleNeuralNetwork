8760ebe581885cb59fc163655d50ed8b5205f683
Louis-Saglio <louis.saglio@sfr.fr>
2019-03-09 00:34:19 +0100

    Fix bug in Being.decode Make genome a constructor parameter Remove useless sigmoid function usage

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,33 +1,35 @@
-import math
+"""
+Attempt to build a more biologic inspired genetic algorithm data-structure module
+"""
 
-from neural_network import Network, Neuron
-
-
-def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+from neural_network import Perceptron, NeuralNetwork
+from utils import rpprint
 
 
 class Being:
-    def __init__(self):
-        self.genome = (["4589", "7415", "212", "089", "277", "0254"], sigmoid)
-        self.network = Network(self.decode())
-        print(self.network.layers)
+    def __init__(self, genome):
+        self.genome = genome
+        self.network = NeuralNetwork(self.decode())
 
     def decode(self):
         network = []
+        previous_gene_size = 0
         layer = []
-        size = len(self.genome[0][0])
         for gene in self.genome[0]:
-            if len(gene) != size:
-                network.append(layer)
+            if previous_gene_size != len(gene):
+                if layer:
+                    network.append(layer)
                 layer = []
-            layer.append(Neuron(int(gene[0]), [int(weight) for weight in gene[1:]], self.genome[1]))
+            layer.append(Perceptron(int(gene[0]), [int(weight) for weight in gene[1:]], self.genome[1]))
+            previous_gene_size = len(gene)
         network.append(layer)
         return network
 
 
 def test():
-    b = Being()
+    b = Being((["4589", "7415", "212", "089", "277", "0254"], lambda x: x))
+    rpprint(b.network)
+    assert b.network.run((-1, 0, 1)) == [1188]
 
 
 if __name__ == "__main__":
