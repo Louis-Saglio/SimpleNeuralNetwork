c6edcd89729a9d7bb3eecd143b7ff5c15b6ea1b5
Louis-Saglio <louis.saglio@sfr.fr>
2019-03-14 23:56:00 +0100

    Use matplotlib to display evolution graphs

--- evaluation.py
+++ evaluation.py
@@ -4,9 +4,7 @@ from random import randint
 
 def evaluate_and(individual, nbr=100):
     total = 0
-    for _ in range(nbr):
-        a = randint(0, 1)
-        b = randint(0, 1)
+    for a, b in ((0, 0), (0, 1), (1, 0), (1, 1)):
         c = bool(a) and bool(b)
         if (round(individual(a, b)[0]) % 2 == 0) is c:
             pass
@@ -23,8 +21,7 @@ def evaluate_sum(individual, nbr=10):
         a = randint(-100, 100)
         b = randint(-100, 100)
         c = a + b
-        if not c == round(individual(a, b)[0]):
-            total += 1
+        total += abs(c - individual(a, b)[0])
     return total / nbr
 
 
@@ -34,4 +31,14 @@ def evaluate_sqrt(individual, nbr=300):
         a = randint(0, 1000)
         c = round(sqrt(a))
         total += abs(c - individual(a)[0])
+    return total / nbr
+
+
+def evaluate_is_divisible_by(individual, divider=10, nbr=100):
+    total = 0
+    for _ in range(nbr):
+        a = randint(0, 1000)
+        c = a % divider == 0
+        if (round(individual(a)[0]) % 2 == 0) is c:
+            total += 1
     return total / nbr
--- playground.py
+++ playground.py
@@ -1,17 +1,21 @@
 from random import randint
 from statistics import mean
 
-from evaluation import evaluate_sum, evaluate_sqrt
+from evaluation import evaluate_sum, evaluate_sqrt, evaluate_and, evaluate_is_divisible_by
 from evolution import get_random_neural_network_population
+from matplotlib import pyplot as plt
 
-gen_nbr = 2000
+gen_nbr = 100
 
 # generate
 pop_size = 2000
-population = get_random_neural_network_population(pop_size, 1, 1, lambda x: x)
+population = get_random_neural_network_population(pop_size, 2, 1, lambda x: x)
 
 
-evaluate = evaluate_sqrt
+evaluate = evaluate_sum
+
+avg_list = []
+best_list = []
 
 for _ in range(gen_nbr):
     try:
@@ -24,9 +28,19 @@ for _ in range(gen_nbr):
         population = sorted(population, key=lambda individual: evaluate(individual))
 
         scores = [evaluate(individual) for individual in population]
-        print(round(mean(scores), 2), round(min(scores), 2))
+        avg = round(mean(scores), 2)
+        best = round(min(scores), 2)
+        # print(avg, best)
+        avg_list.append(avg)
+        best_list.append(best)
     except Exception as e:
         print(e)
         # data corruption may happen
         if input("Continue ? (y/n)") == "n":
             break
+
+plt.plot(avg_list[10:])
+plt.show()
+
+plt.plot(best_list)
+plt.show()
